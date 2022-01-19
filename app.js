914ede619db06c637f3bbee79f6ec190591d23dd
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2022-01-19 09:48:14 +0530

    encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -16,12 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema( {
   email: String,
   password: String
-};
+});
 
-
+const secret = "ThisisourlittleSecret.";
+// userSchema.plugin(encrypt, { secret: secret });//this line of code encrypt whole database
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
