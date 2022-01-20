60a4658641d45498408e8e1db34123d40fc6f822
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2022-01-20 18:07:06 +0530

    hashing of password

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
-
+const md5 = require('md5');
+//hash fxn always gen the same hashTeXt for the same input set
 const app = express();
 
 console.log(process.env.API_KEY);
@@ -22,12 +22,7 @@ const userSchema = new mongoose.Schema( {
   password: String
 });
 
-// MOVED THT SECRET KEY TO THE Env file
-// userSchema.plugin(encrypt, { secret: secret });//this line of code encrypt whole database
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
-
 
 
 app.get("/", function (req, res) {
@@ -52,7 +47,7 @@ app.listen(3000, function () {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function (err) {
@@ -66,7 +61,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
 
   User.findOne({ email: username }, function (err, foundUser) {
